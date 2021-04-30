7d7a693f47d429869a412478433e5b6d0481c6b6
kaloklijk <75416686+kaloklijk@users.noreply.github.com>
2021-04-30 20:24:35 +0800

    Create inverse.py

---
+++ inverse.py
@@ -0,0 +1,11 @@
+import sys
+
+def inverse(a, b):
+  """Print a^-1 (mod b)"""
+  if a == 1:
+    return 1
+  return (b*(a-inverse(b%a, a))+1)/a
+
+if __name__ == "__main__":
+  a, b = int(input("write down a for a^-1(mod b)\n")), int(input("write down b for a^-1(mod b)\n"))
+  print(f"The result is {inverse(a, b)}")
