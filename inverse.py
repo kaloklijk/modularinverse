a080a28900bce29c1c9c812ff70a2f6eb7b9998a
Li Ka Lok <75416686+kaloklijk@users.noreply.github.com>
2021-06-07 16:14:23 +0800

    Update inverse.py

--- inverse.py
+++ inverse.py
@@ -4,8 +4,11 @@ def inverse(a, b):
   """Print a^-1 (mod b)"""
   if a == 1:
     return 1
+  if a == 0:
+    print("Inverse doesn't exist!\n")
+    return 0
   return (b*(a-inverse(b%a, a))+1)/a
 
 if __name__ == "__main__":
   a, b = int(input("write down a for a^-1(mod b)\n")), int(input("write down b for a^-1(mod b)\n"))
-  print(f"The result is {inverse(a%b, b)}")
+  print(f"The result is {inverse(a, b)}")
