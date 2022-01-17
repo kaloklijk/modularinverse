f8ed192519f9d21e7b793c479dc2496bdd94586f
Li Ka Lok <75416686+kaloklijk@users.noreply.github.com>
2022-01-17 14:48:22 +0800

    Update inverse.py

--- inverse.py
+++ inverse.py
@@ -1,14 +1,37 @@
+'''
+The new algorithm developed by Li Ka Lok for computing modular multiplicative inverses and show
+the computation from step to step using networkx directed graphs
+'''
+## import the required library
 import sys
+import networkx as nx
+import matplotlib.pyplot as plt
 
-def inverse(a, b):
-  """Print a^-1 (mod b)"""
-  if a == 1:
-    return 1
-  if a == 0:
-    print("Inverse doesn't exist!\n")
-    return 0
-  return (b*(a-inverse(b%a, a))+1)/a
+## The inversing algorithm a*a^-1 = b*(-b)^-1(mod a) + 1
+def inverse(a, b, g, h):
+    '''
+    Compute the modular inverse of a mod b
+    '''
+    ## show the procedure in a graph
+    g.add_node(a)
+    h.add_node(b)
+    x = [n for n in g.nodes if g.out_degree(n) == 0]
+    y = [n for n in h.nodes if h.out_degree(n) == 0]
+    if x[0] != a: g.add_edge(x[0], a)
+    if y[0] != b: h.add_edge(y[0], b)
+    ## actually computing the function
+    return (b*(a-inverse(b%a, a, g, h))+1)/a if a%b != 1 else 1
 
-if __name__ == "__main__":
-  a, b = int(input("write down a for a^-1(mod b)\n")), int(input("write down b for a^-1(mod b)\n"))
-  print(f"The result is {inverse(a, b)}")
+## driver code
+g = nx.DiGraph()
+h = nx.DiGraph()
+a = int(input("input value a:"))
+b = int(input("input value b:"))
+print(f"The modular multiplcative inverse of a mod b is {inverse(a, b, g, h)}")
+posg = nx.kamada_kawai_layout(g)
+posh = nx.kamada_kawai_layout(h)
+plt.figure(f"values of a")
+nx.draw(g, posg, with_labels = True, edge_color = "black", node_color = "red")
+plt.figure(f"values of b")
+nx.draw(h, posh, with_labels = True, edge_color = "black", node_color = "red")
+plt.show()
